c18e5c9c8550c7dc8b06e01e40d09669ae0d3d91
Coder7Raj <alwaysraj733@gmail.com>
2026-02-06 20:56:43 +0600

    create the thought model that user gonna post

---
+++ thoughts-backend/models/thought.model.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+
+const categories = [
+  "Life",
+  "Technology",
+  "Programming",
+  "Career",
+  "Motivation",
+  "Personal Growth",
+  "Education",
+  "Health",
+  "Travel",
+  "Opinions",
+];
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    country: {
+      type: String,
+      default: "",
+    },
+
+    category: {
+      type: String,
+      enum: categories,
+      required: true,
+    },
+
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+thoughtSchema.index({ createdAt: -1 });
+thoughtSchema.index({ likes: -1 });
+
+export default mongoose.model("Thought", thoughtSchema);
