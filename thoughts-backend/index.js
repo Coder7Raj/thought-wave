d13b672e48610b06b223c91cdd0e323edffbf151
Coder7Raj <alwaysraj733@gmail.com>
2026-02-08 22:49:23 +0600

    update root file and packages

--- thoughts-backend/index.js
+++ thoughts-backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import thoughtRouter from "./routes/thought.routes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/thought", thoughtRouter);
 
 app.listen(PORT, () => {
   connectDB();
